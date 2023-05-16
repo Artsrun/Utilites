802b24a14f18fcb518b90288daa93c0856c065d2
Artsrun Hakobyan <artsrunhakobyann@gmail.com>
2023-05-16 21:13:34 +0400

    Update utilites.js

--- utilites.js
+++ utilites.js
@@ -1,19 +1,25 @@
+(()=>{
+  
 const imagesNodeList = document.querySelectorAll("img");
 imagesNodeList.forEach((image) => {
-  const src = image.src;
-  const imageName = src.split("/").pop();
-
-  fetch(src)
-    .then((response) => response.blob())
-    .then((blob) => {
-      const file = new File([blob], imageName);
-      const url = URL.createObjectURL(file);
-
-      const link = document.createElement("a");
-      link.href = url;
-      link.download = imageName;
-      link.click();
-
-      URL.revokeObjectURL(url);
-    });
+    const src = image.src;
+    const imageName = src.split("/").pop().split("?")[0];  // Remove the part after ?
+    fetch(src)
+        .then(response => response.blob())
+        .then(blob => {
+            const url = URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            const timestamp = new Date().getTime();
+            const fileNameParts = imageName.split('.');
+            const fileExtension = fileNameParts.pop();
+            const fileName = fileNameParts.join('.');
+            const modifiedFileName = `${fileName}_${timestamp}.${fileExtension}`;
+            link.download = modifiedFileName;
+            link.click();
+        })
+        .catch(console.error);
 });
+
+
+})()
